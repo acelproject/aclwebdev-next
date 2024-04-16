bb0826d5d0600272ffdcf2020185acff8bb1f9c5
acelproject <aclprcjt@gmail.com>
2024-04-16 17:30:17 +0800

    pagination style

--- components/Reviews.tsx
+++ components/Reviews.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useCallback, useRef } from "react";
 import {
   Card,
   CardDescription,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import {  Send } from "lucide-react";
+import { Send } from "lucide-react";
 
 // import swiper styles
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -13,13 +13,13 @@ import "swiper/css";
 import "swiper/css/pagination";
 
 // import required modules
-import { Pagination } from "swiper/modules";
+import { Pagination, Navigation } from "swiper/modules";
 import Image from "next/image";
 import useSWR from "swr";
 import Link from "next/link";
 
 // image
-import noAvatar from "@/public/noavatar.png"
+import noAvatar from "@/public/noavatar.png";
 
 // const reviewData = [
 //   {
@@ -71,9 +71,21 @@ const Reviews = () => {
     `${process.env.NEXT_PUBLIC_API_URL}/api/reviews`,
     fetcher
   );
+
+  const sliderRef = useRef(null);
+
+  const handlePrev = useCallback(() => {
+    if (!sliderRef.current) return;
+    sliderRef.current.swiper.slidePrev();
+  }, []);
+
+  const handleNext = useCallback(() => {
+    if (!sliderRef.current) return;
+    sliderRef.current.swiper.slideNext();
+  }, []);
   return (
     <section className="mb-12 xl:mb-32">
-      <div className="container mx-auto">
+      <div className="container mx-auto relative">
         <h2 className="section-title mb-12 text-center mx-auto">Reviews</h2>
         {/* Slider */}
         <Swiper
@@ -83,14 +95,14 @@ const Reviews = () => {
             1400: { slidesPerView: 3 },
           }}
           spaceBetween={30}
-          modules={[Pagination]}
-          pagination={{ clickable: true }}
-          className="h-max "
+          modules={[Navigation,Pagination]}
+          pagination={{clickable: true,el:".my-pagination"}}
+          className="h-max"
         >
           {data?.data.map((person: any, i: any) => {
             return (
               <SwiperSlide key={i}>
-                <Card className="bg-white dark:bg-secondary/40 p-8 min-h-max">
+                <Card className="bg-white dark:bg-secondary/40 p-8 min-h-[250px]">
                   <CardHeader>
                     <div className="flex items-center gap-x-4">
                       {/* image */}
@@ -116,11 +128,17 @@ const Reviews = () => {
               </SwiperSlide>
             );
           })}
+          <div className="md:mt-10 mt-7 flex justify-center gap-x-5">
+            <div className="my-pagination w-auto mx-0 bg-transparent flex gap-x-2 "></div>
+          </div>
         </Swiper>
-        <div className="flex justify-center mt-12">
-          <Link href={`/reviews`} className="flex gap-x-3 items-center px-6 py-4 text-lg dark:bg-secondary dark:text-slate-300 dark:hover:bg-primary dark:hover:text-white text-slate-700 hover:bg-primary bg-slate-200 font-normal transition-all duration-200 rounded-full hover:text-white border-none shadow-lg">
+        <div className="flex justify-center mt-7 md:mt-12">
+          <Link
+            href={`/reviews`}
+            className="flex gap-x-3 items-center px-4 md:px-6 py-3 md:py-4 md:text-lg tetxt-sm dark:bg-secondary dark:text-slate-300 dark:hover:bg-primary dark:hover:text-white text-slate-700 hover:bg-primary bg-slate-200 font-normal transition-all duration-200 rounded-full hover:text-white border-none shadow-lg"
+          >
             Create review
-            <Send size={18} />
+            <Send size={14} />
           </Link>
         </div>
       </div>
